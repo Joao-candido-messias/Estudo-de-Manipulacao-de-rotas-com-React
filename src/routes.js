b281fb9cc7501c7acb14095b8482069066824c41
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-09-01 23:42:52 -0300

    Manipulação de rotas com tag Link e Uselocation

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobremim";
+import Menu from "./componentes/menu";
 
 
-function App() {
+
+function AppRoutes() {
   return(
     <BrowserRouter>
+    <Menu/>
       <Routes>
         <Route path="/" element={<Inicio/>}/>
         <Route path="/sobremim" element={<SobreMim/>}/>
@@ -16,4 +18,4 @@ function App() {
   )
 }
 
-export default App;
+export default AppRoutes;
